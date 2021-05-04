3b57088b3af0c482750448681cbabb56fa5cad12
chayadeaver <chayadeaver@gmail.com>
2021-05-04 19:55:02 -0400

    add test case for exists and uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe User, type: :model do
   let (:user) { User.create!(name: "Chaya", email: "chaya@email.com", password: "password") }
+
+  it "exists" do
+    expect { User.new }.to_not raise_error
+  end
     
   it "has a name" do
     expect(user.name).to eq("Chaya")
@@ -11,4 +16,10 @@ RSpec.describe User, type: :model do
     expect(user.email).to eq("chaya@email.com")
   end
 
+  it "should have a unique email" do
+    User.create(name: "some user", email: "test@email.com", password: "password")
+    user1 = User.new(name: "another user", email: "test@email.com", password: "password")
+    user1.valid?
+    expect(user1.errors[:email]).to include("has already been taken")
+  end
 end
