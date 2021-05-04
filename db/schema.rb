318c5f0347855cf5d5770fcfe312b62edf86eee6
chayadeaver <chayadeaver@gmail.com>
2021-05-03 21:50:32 -0400

    add name property to users

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_05_03_143758) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
