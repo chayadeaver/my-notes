dd92ad71f7ba332fd06fedec133c6f95ad251df4
chayadeaver <chayadeaver@gmail.com>
2021-05-03 21:09:51 -0400

    set up root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root to: "home#welcome"
   resources :notes
   devise_for :users, controllers: {registrations: 'registrations'}
-  
+  devise_scope :user do
+    get '/login', to: 'devise/sessions#new'
+    get '/signup', to: 'devise/registrations#new'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
