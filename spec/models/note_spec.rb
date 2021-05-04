b81dafac3b6761bf772a0ae03a4b7e9771dbfe85
chayadeaver <chayadeaver@gmail.com>
2021-05-04 15:17:06 -0400

    incorporate context

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -3,34 +3,42 @@ require 'rails_helper'
 RSpec.describe Note, type: :model do
   before do
     @user = User.create!(name: "Chaya", email: "chaya@email.com", password: "password")
-    @note1 = @user.notes.create!(title: "to do something")
-    @note2 = @user.notes.create!(body: "not to do something")
-    
   end
+
+  context "with a title" do
+    let(:note) { @user.notes.create!(title: "to do something") }
+
+    it "can have a title without a body" do
+      expect(note).to be_valid
+    end
+
+    it "title should not be longer than 30 characters" do
+      expect(note).to be_valid
+    end
+
+    it "should have a user" do
+      expect(note.user.name).to eq("Chaya")
+    end
   
-  it "should have a user" do
-    expect(@note1.user.name).to eq("Chaya")
+    it "can't have empty title or body" do
+      note.title = nil
+      note.body = nil
+      expect(note).to_not be_valid
+    end
+
   end
 
-  it "can't have empty title or body" do
-    @note1.title = nil
-    @note1.body = nil
-    expect(@note1).to_not be_valid
+  context "with a body" do
+    let(:note) { @user.notes.create!(body: "not to do something") }
+
+    it "can have a body without a title" do
+      expect(note).to be_valid
+    end
+  
+    it "body should not be longer than 1000 characters" do
+      expect(note).to be_valid
+    end
   end
 
-  it "can have a title without a body" do
-    expect(@note1).to be_valid
-  end
-
-  it "can have a body without a title" do
-    expect(@note2).to be_valid
-  end
-
-  it "title should not be longer than 30 characters" do
-    expect(@note1).to be_valid
-  end
-
-  it "body should not be longer than 1000 characters" do
-    expect(@note2).to be_valid
-  end
+  
 end
