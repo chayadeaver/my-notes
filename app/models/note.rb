26225725edc648bf02750dc826b471f4a7f5a3a4
chayadeaver <chayadeaver@gmail.com>
2021-05-04 14:29:30 -0400

    add validations for note

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,8 @@
 class Note < ApplicationRecord
   belongs_to :user, optional: true
+  validates :title, presence: true, unless: -> (note){note.body.present?}
+  validates :title, length: { maximum: 30, too_long: "%{count} characters is the maximum allowed" }
+  validates :body, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }, allow_blank: true
+  
+
 end
