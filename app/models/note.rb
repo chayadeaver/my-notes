4d5a5a4a6d3242a16ccb8f4d4a4e220a4700c653
chayadeaver <chayadeaver@gmail.com>
2021-05-05 19:22:49 -0400

    has_rich_text for body

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,5 @@
 class Note < ApplicationRecord
+  has_rich_text :body
   belongs_to :user, optional: true
   validates :title, presence: true, unless: -> (note){note.body.present?}
   validates :title, length: { maximum: 30, too_long: "%{count} characters is the maximum allowed" }
