9aa8ac1c8eeecf9f5297473368d9ea2b44c39644
chayadeaver <chayadeaver@gmail.com>
2021-05-05 11:49:38 -0400

    changed login to signin

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :notes
   devise_for :users, controllers: {registrations: 'registrations'}
   devise_scope :user do
-    get '/login', to: 'devise/sessions#new'
+    get '/signin', to: 'devise/sessions#new'
     get '/signup', to: 'devise/registrations#new'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
