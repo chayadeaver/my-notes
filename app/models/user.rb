e0440d2b97023e0e0a82aa6c720987c05f731fbc
chayadeaver <chayadeaver@gmail.com>
2021-05-06 20:29:16 -0400

    removed mailer codes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :notes, dependent: :destroy
   validates :email, :name, presence: true
   validates_uniqueness_of :email
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,9 +97,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'my-notes-2021.herokuapp.com', protocol: "https" }
-
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
